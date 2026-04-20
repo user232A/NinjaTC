4694d7b3907bc3c33555f7a2ad8e7e6d57d6b4a9
alexandr0202 <230224024+user232A@users.noreply.github.com>
2026-04-20 17:38:44 +0300

    add Config.java

---
+++ src/test/java/api/configs/Config.java
@@ -0,0 +1,39 @@
+package api.configs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+
+    private static final Config INSTANCE = new Config();
+    private final Properties properties = new Properties();
+
+    private Config() {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new RuntimeException("config.properties not found in resources");
+            }
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("fail to load config.properties");
+        }
+    }
+
+    public static String getProperty(String key) {
+        String systemValue = System.getProperty(key);
+
+        if (systemValue != null){
+            return systemValue;
+        }
+
+        String envKey = key.toUpperCase().replace(".", "");
+        String envValue = System.getenv(envKey);
+
+        if (envValue != null){
+            return envValue;
+        }
+
+        return INSTANCE.properties.getProperty(key);
+    }
+}
