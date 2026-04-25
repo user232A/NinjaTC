6d78d5149b0183b6574aad57ec4faaebaa59d0f7
durova-nadya <joyhope@yandex.ru>
2026-04-25 11:50:24 +0300

    Resolve merge conflict in Endpoint enum

--- src/test/java/api/requests/skeleton/Endpoint.java
+++ src/test/java/api/requests/skeleton/Endpoint.java
@@ -8,21 +8,26 @@ import api.models.response.CreateVcsRootResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Map;
+
 @Getter
 @AllArgsConstructor
 public enum Endpoint {
     USERS(
             "/app/rest/users",
             CreateUserRequest.class,
-            CreateUserResponse.class
+            CreateUserResponse.class,
+            Map.of()
     ),
     VCS_ROOTS(
             "/app/rest/vcs-roots",
             CreateVcsRootRequest.class,
-            CreateVcsRootResponse.class
+            CreateVcsRootResponse.class,
+            Map.of()
     );
 
     private final String url;
     private final Class<? extends BaseModel> requestModel;
     private final Class<? extends BaseModel> responseModel;
+    private final Map<String, Class<String>> pathParam;
 }
